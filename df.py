664fc58d1a060b8fef4afbe7410944ab6c7d5b4a
zjzhai <apl.359@163.com>
2011-12-13 07:49:06 +0800

    generate a dbunit item

---
+++ df.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+ 
+import json
+
+import codecs
+where = 'e:/pyTest/result.txt'
+
+template = '<areas id=\"%d\" version=\"%s\" code=\"%s\" name=\"%s\" parent=\"%s\" level=\"%s\"/>\r\n'  
+
+result_str = ""
+
+fc = ""
+
+id = 0
+
+f = codecs.open('e:/pyTest/areas.json','r', 'utf8');
+
+fc = f.read()
+
+f.close()
+
+js = json.loads(str(fc))
+
+for pro in js['province']:
+    id = id+1
+    result_str += template % (id, '1', pro['code'][0:2], pro['name'], '00', 0 )
+
+for pro in js['city']:
+    id = id+1
+    result_str += template % (id, '1', pro['code'][0:4], pro['name'], pro['code'][0:2] , 1)
+
+for pro in js['district']:
+    id = id+1
+    result_str += template % (id, '1', pro['code'][0:6], pro['name'], pro['code'][0:4] ,2)
+
+open(where, 'w').write(result_str)
+
